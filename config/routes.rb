819bf561e045591aaa85be3e8e6dbcbbf942f7ab
Kelvin Ndoma <kelvinndomamutua@gmail.com>
2025-01-07 18:24:20 +0300

    added admin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  get "home/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Devise routes for admins
+  devise_for :admins
+  
+  # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
+  # Public root
   root "home#index"
+
+  # Admin-specific root for authenticated admins
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  # Admin dashboard
+  get "admin" => "admin#index"
 end
